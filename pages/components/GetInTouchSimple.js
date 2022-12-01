bf29dafe56ac06101b79b5ffa6ecdbd66035842c
VerduijnPujol98 <asukaleenverduijn@gmail.com>
2022-12-01 22:23:15 +0100

    Fixed errors

--- pages/components/GetInTouchSimple.js
+++ pages/components/GetInTouchSimple.js
@@ -1,80 +1,9 @@
-import { TextInput, Textarea, SimpleGrid, Group, Title, Button, Container } from '@mantine/core';
-import { useForm } from '@mantine/form';
-
+import React from 'react'
 
 const GetInTouchSimple = () => {
-  const form = useForm({
-    initialValues: {
-      name: '',
-      email: '',
-      subject: '',
-      message: '',
-    },
-    validate: {
-      name: (value) => value.trim().length < 2,
-      email: (value) => !/^\S+@\S+$/.test(value),
-      subject: (value) => value.trim().length === 0,
-    },
-  });
-
   return (
-    <Container mt={200}>
-    <form onSubmit={form.onSubmit(() => {})}>
-      <Title
-        order={2}
-        fz={70}
-        sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}` })}
-        weight={900}
-        align="center"
-      >
-        Kontakt Os
-      </Title>
-
-      <SimpleGrid cols={2} mt="xl" breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
-        <TextInput
-          label="Name"
-          placeholder="Your name"
-          name="name"
-          variant="filled"
-          {...form.getInputProps('name')}
-        />
-        <TextInput
-          label="Email"
-          placeholder="Your email"
-          name="email"
-          variant="filled"
-          {...form.getInputProps('email')}
-        />
-      </SimpleGrid>
-
-      <TextInput
-        label="Subject"
-        placeholder="Subject"
-        mt="md"
-        name="subject"
-        variant="filled"
-        {...form.getInputProps('subject')}
-      />
-      <Textarea
-        mt="md"
-        label="Message"
-        placeholder="Your message"
-        maxRows={10}
-        minRows={5}
-        autosize
-        name="message"
-        variant="filled"
-        {...form.getInputProps('subject')}
-      />
-
-      <Group position="center" mt="xl">
-        <Button variant='outline' color='dark' type="submit" size="md">
-          Send Besked
-        </Button>
-      </Group>
-    </form>
-    </Container>
-  );
+    <div>GetInTouchSimple</div>
+  )
 }
 
 export default GetInTouchSimple
